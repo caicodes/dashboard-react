93c7878dd457c385fd02e9f0f3645c721e044460
caicodes <caicodes1@gmail.com>
2022-08-26 14:46:41 -0600

    starter for dashboards with react and charting libs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="App-msg">
+          dashboard delightfulness in dark only mode... with D3, charts, and
+          whatever I can get to have dynamic bad ass dashboards live in my
+          portfolio... now!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
