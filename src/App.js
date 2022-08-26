de17029b47b2fc163b18b39dd270fe83c53c7c47
caicodes <caicodes1@gmail.com>
2022-08-26 15:11:21 -0600

    daisyui added and implemented, gsap added, not implemented

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import ReactLogo from "./components/ReactLogo.svg";
 
 function App() {
   return (
-    <div className="App bg-fuchsia-900 min-h-screen text-center text-2xl text-white">
-      <header className="App-header text-fuchsia-700 animate-pulse">
+    <div className="App min-h-screen text-center text-2xl ">
+      <header className="App-header text-primary animate-pulse">
         <ReactLogo />
         <p className="App-msg mx-auto w-96 ">
           dashboard delightfulness in dark only mode... with D3, charts, and
           whatever I can get to have dynamic bad ass dashboards live in my
           portfolio... now!
         </p>
+        <button className="btn btn-xl mt-8">hello daisyui</button>
       </header>
     </div>
   );
